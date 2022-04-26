515a369ad7483287cbdc0dcfaf878970b0f443ce
vilalife <79405067+Angel-Sweetie-Pie@users.noreply.github.com>
2022-04-26 18:55:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+console.log("Hi this is the console log");
+
+var first_name="Delisha";
+console.log(first_name);
+
+var last_name="Makwana";
+console.log(last_name);
+
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+
+var x = 100;
+var y = 150;
+console.log(x + y);
+
+var maximum_number=Math.max(15, 13, 25, 1);
+console.log(maximum_number);
+
+var minimum_number=Math.min(15, 13, 25, 1);
+console.log(minimum_number);
